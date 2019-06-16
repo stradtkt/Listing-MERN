baf95a03b70cbc980408eac0ff38efbc8d6c199c
Kevin <stradtkt22@gmail.com>
2019-06-15 20:38:58 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({extended: false}));
+
+app.get('/', (req, res) => {
+    res.send('API Running');
+});
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/listing', require('./routes/api/listing'));
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
